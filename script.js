672892b2e17c195b5c2437a8849a8adbcd47e6b0
shiva-saini <shivasainisiyana2211@gmail.com>
2023-06-17 16:57:49 +0530

    done

--- script.js
+++ script.js
@@ -8,20 +8,59 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+ let employees =  arr.map((obj) =>{
+    
+     if(obj.profession === "developer"){
+      console.log(obj.name);
+     }
+  })
+  
+  
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((obj) => {
+    if(obj.profession === "developer"){
+      console.log(obj.name);
+     }
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+  let data = {id:4,name:"susan",age:"20",profession:"intern"};
+  arr.push(data);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  let updated = arr.filter((obj) => {
+        return obj.profession !== "admin";
+  })
+  arr = updated
+  console.log(updated);
+  // console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let anotherArray = [
+    { id: 5, name: "leo", age: "20", profession: "admin" },
+    { id: 6, name: "tdz", age: "23", profession: "intern" },
+    { id: 7, name: "joe", age: "26", profession: "admin" },
+  ]
+
+  let arr1 = [...arr,...anotherArray];
+  console.log(arr1);
 }
+
+// PrintDeveloperbyMap();
+
+// PrintDeveloperbyForEach();
+
+// addData();
+// removeAdmin();
+
+// concatenateArray();
